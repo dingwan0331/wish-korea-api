d3ccb27714151813ad90d3717211e85867b09895
dingwan <dingwan0331@gmail.com>
2022-10-01 19:12:05 +0900

    test: users_signup null true testcase 추가

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,5 @@
 import json
+from urllib import request
 
 from django.test import TestCase, Client
 
@@ -21,8 +22,25 @@ class SignUpTest(TestCase):
             'address'      : '서울특별시 강남구 대치동',
             'nick_name'    : 'coding'
             }
-        request_body_json = request_body
-        response = client.post('/users/signup', request_body_json, content_type='application/json')
+
+        response = client.post('/users/signup', request_body, content_type='application/json')
+
+        self.assertEqual(response.json(), {'message' : 'Created'})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response['content-type'], 'application/json')
+    
+    def test_success_case_with_null_true(self):
+        client = Client()
+        request_body = {
+            'username'     : 'ding',
+            'password'     : '123123a!',
+            'email'        : 'ding123@gmail.com',
+            'phone_number' : '010-1234-1234',
+            'first_name'   : '정',
+            'last_name'    : '코드'
+            }
+
+        response = client.post('/users/signup', request_body, content_type='application/json')
 
         self.assertEqual(response.json(), {'message' : 'Created'})
         self.assertEqual(response.status_code, 201)
