cbc965faf3e66ef1ab5f29d41b7bcea32a8f570f
dingwan <dingwan0331@gmail.com>
2022-10-03 19:43:53 +0900

    test: users_signin success testcase 추가

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,11 @@
 import json
-from urllib import request
+import bcrypt
+import jwt
 
 from django.test import TestCase, Client
 
-from .models import User
+from .models             import User
+from wish_korea.settings import SECRET_KEY, ALGORITHM
 
 # Create your tests here.
 class SignUpTest(TestCase):
@@ -45,3 +47,36 @@ class SignUpTest(TestCase):
         self.assertEqual(response.json(), {'message' : 'Created'})
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response['content-type'], 'application/json')
+
+class SignUpTest(TestCase):
+    def setUp(self) -> None:
+        password = '123123a!'
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+        User.objects.create(
+            id           = 1,
+            username     = 'ding',
+            password     = hashed_password,
+            email        = 'ding123@gmail.com',
+            phone_number = '010-1234-1234',
+            first_name   = '정',
+            last_name    = '코드'
+        )
+
+    def tearDown(self) -> None:
+        User.objects.all().delete()
+    
+    def test_success_case(self):
+        client = Client()
+        request_body = {
+            'username'     : 'ding',
+            'password'     : '123123a!',
+        }
+
+        response = client.post('/users/signin', request_body, content_type='application/json')
+
+        token = jwt.encode({'user_id': 1}, SECRET_KEY, ALGORITHM)
+
+        self.assertEqual(response.json(), {'message' : 'Success'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertEqual(response['Authorization'], token)
