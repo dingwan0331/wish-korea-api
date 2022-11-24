986bb3f9d8ed5ca13bec1b8f903f2449293a8f25
dingwan <dingwan0331@gmail.com>
2022-11-24 20:06:37 +0900

    refactor: User 클래스에 refresh_token 속성 추가

--- users/models.py
+++ users/models.py
@@ -3,14 +3,15 @@ from django.db import models
 from core.models import TimeStampModel
 
 class User(TimeStampModel):
-    username     = models.CharField(max_length = 50, unique = True)
-    password     = models.CharField(max_length = 200)
-    email        = models.CharField(max_length = 100, unique = True)
-    phone_number = models.CharField(max_length = 50, unique = True)
-    first_name   = models.CharField(max_length = 50)
-    last_name    = models.CharField(max_length = 50)
-    address      = models.CharField(max_length = 200, default = '')
-    nick_name    = models.CharField(max_length = 50, default = '')
+    username      = models.CharField(max_length = 50, unique = True)
+    password      = models.CharField(max_length = 200)
+    email         = models.CharField(max_length = 100, unique = True)
+    phone_number  = models.CharField(max_length = 50, unique = True)
+    first_name    = models.CharField(max_length = 50)
+    last_name     = models.CharField(max_length = 50)
+    address       = models.CharField(max_length = 200, default = '')
+    nick_name     = models.CharField(max_length = 50, default = '')
+    refresh_token = models.CharField(max_length = 150, null = True)
 
     class Meta:
         db_table = 'users'
