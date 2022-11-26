f08d792b0f1f16a58a1dada5aa58a466378949aa
dingwan <dingwan0331@gmail.com>
2022-11-26 19:39:08 +0900

    refactor: User 모델클래스 리펙토링
    - refresh_token 속성 삭제
    - first_name, last_name 을 name속성으로 통합
    - default '' 를 null True로 교체
    - password BinaryField로 변경 및 max_length 60으로 수정

--- auth/models.py
+++ auth/models.py
@@ -4,14 +4,12 @@ from core.models import TimeStampModel
 
 class User(TimeStampModel):
     username      = models.CharField(max_length = 50, unique = True)
-    password      = models.CharField(max_length = 200)
+    password      = models.BinaryField(max_length = 60)
     email         = models.CharField(max_length = 100, unique = True)
-    phone_number  = models.CharField(max_length = 50, unique = True)
-    first_name    = models.CharField(max_length = 50)
-    last_name     = models.CharField(max_length = 50)
-    address       = models.CharField(max_length = 200, default = '')
-    nick_name     = models.CharField(max_length = 50, default = '')
-    refresh_token = models.CharField(max_length = 150, null = True)
+    phone_number  = models.CharField(max_length = 13, unique = True)
+    name          = models.CharField(max_length = 5)
+    address       = models.CharField(max_length = 200, null = True)
+    nick_name     = models.CharField(max_length = 15, null = True)
 
     class Meta:
         db_table = 'users'
