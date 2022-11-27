134c413f8e36a9c054e3c563c4cf02f2d82ce754
dingwan <dingwan0331@gmail.com>
2022-11-27 16:25:21 +0900

    refactor : refresh 토큰 로직 수정
    - 리프레시 토큰 기반 액세스 토큰 재발급 엔드포인트 지정
    - TokenView로 따로 분리
    - 토큰 access_token 만료시간 1시간으로 변경
    - 재발급 메서드 resign_token 으로 변경
    - 레디스 캐시 관련부분 삭제

--- core/token.py
+++ core/token.py
@@ -2,16 +2,13 @@ import jwt
 
 from datetime import datetime, timedelta
 
-from django.core.cache import cache
-from django.http       import JsonResponse
-
 from my_settings import SECRET_KEY, REFRESH_TOKEN_SECRET_KEY, ACCESS_TOKEN_ALGORITHM, REFRESH_TOKEN_ALGORITHM
 
 class Token:
     def __init__(self, type):
         self.type    = type
         self.EXPIRES = {
-            'access_token': datetime.utcnow() + timedelta(seconds = 1), 
+            'access_token': datetime.utcnow() + timedelta(hours = 1), 
             'refresh_token': datetime.utcnow() + timedelta(hours = 8)
             }
         self.ALGORITHMS  = {'access_token': ACCESS_TOKEN_ALGORITHM , 'refresh_token': REFRESH_TOKEN_ALGORITHM}
@@ -26,15 +23,10 @@ class Token:
 
         return token
 
-    def sign_token_again(self, access_token):
-        try:
-            refresh_token            = cache.get(access_token)
+    def resign_token(self, refresh_token):
             REFRESH_TOKEN_ALGORITHM  = self.ALGORITHMS['refresh_token']
             REFRESH_TOKEN_SECRET_KEY = self.SECRET_KEYS['refresh_token']
             user_id                  = jwt.decode(refresh_token, REFRESH_TOKEN_SECRET_KEY, REFRESH_TOKEN_ALGORITHM)['id']
             access_token             = self.sign_token(user_id)
-
+    
             return access_token
-
-        except jwt.exceptions.ExpiredSignatureError:
-            raise Exception()
